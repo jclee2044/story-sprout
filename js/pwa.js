d13944b9abfa0365145857ae335b4981ba6b4f74
Jacob Lee <jacoblee@Jacobs-MacBook-Air.local>
2026-04-07 17:45:04 -0400

    PWA implementation
    Using conventions as discussed in class

---
+++ js/pwa.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      await navigator.serviceWorker.register("./sw.js");
+    } catch (error) {
+      console.error("Service worker registration failed:", error);
+    }
+  });
+}
