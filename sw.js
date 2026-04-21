b498f8b31cb84bac7633f3adf83967cd3b05d579
Jacob Lee <jacoblee@Jacobs-MacBook-Air.local>
2026-04-21 10:01:06 -0400

    improve print layout and composition

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "story-sprout-v23"; // bumped version to force update
+const CACHE_NAME = "story-sprout-v25"; // bumped version to force update
 
 const ASSETS_TO_CACHE = [
   "./story-form.html",
